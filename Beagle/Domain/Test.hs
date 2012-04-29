97cc82806ffb43d76951beb70de73c950cdd8720
James Hibberd <james.d.hibberd@gmail.com>
2012-04-29 10:38:07 -0700

    Rewritten unit tests to use homogenous gene evaluation.

--- Beagle/Domain/Test.hs
+++ Beagle/Domain/Test.hs
@@ -1,82 +1,53 @@
--- | Simple math-based domain to use in unit-tests.
-
 module Beagle.Domain
     ( Gene(..)
-    , genemap
     , genotypeLength
-    , targetPhenotype -- TODO(jhibberd) Do we need this?
     , mutationsPerGenotype
     , populationSize
     , randomSeed
-    , runner
+    , score
     ) where
 
-import Data.Dynamic
+import Beagle.Eval2
 import qualified Data.Map as Map
 
-genotypeLength =        8   :: Int
-targetPhenotype =       10   :: Int
-mutationsPerGenotype =  1   :: Int
-populationSize =        50  :: Int
-randomSeed =            6   :: Int
+-- | Data types and constants --------------------------------------------------
 
-data Gene = Digit1   -- 1
-          | Digit2   -- 2
-          | Digit3   -- 3
-          | Plus     -- Add two digits together
-          | Minus    -- Subtract one digit from another
-          -- | Stick    -- Concat two digits, eg. f 1 3 = 13
-          | Switch   -- Takes three ints; returns one of two ints, depending on
-                     -- the value of a third.
-          -- | Runner   -- Takes a function and applies it to an arbitrary int.
-                     -- Used for an evaluation function applied to the end of a
-                     -- genotype.
-          -- | Runner'  -- Takes an int and returns the difference between it and a 
-                     -- target int.
-          -- | Runner'' -- Convert final int value to float; useful for testing.
-          | Empty    -- Gene to be ignored; allows for genotypes of 'variable'
-                     -- length.
-          deriving (Ord, Eq, Show, Enum)
+genotypeLength =        15      :: Int
+mutationsPerGenotype =  1       :: Int
+populationSize =        30      :: Int
+randomSeed =            6       :: Int
 
-genemap :: Map.Map Gene (Dynamic, Int)
-genemap = Map.fromList [
-    (Digit1,        (toDyn digit1,      0)),
-    (Digit2,        (toDyn digit2,      0)),
-    (Digit3,        (toDyn digit3,      0)),
-    (Plus,          (toDyn plus,        2)),
-    (Minus,         (toDyn minus,       2)),
-    --(Stick,         (toDyn stick,       2)),
-    (Switch,        (toDyn switch,      3))
-    --(Runner,        (toDyn runner,      1)),
-    --(Runner',       (toDyn runner',     1)),
-    --(Runner'',      (toDyn runner'',    1))
+data Gene = Add | Subtract | Multiply | Divide | Incr | Decr | Empty
+    deriving (Ord, Eq, Show, Enum)
+type State = (Int, Int)
+
+gmap :: Map.Map Gene ([Gene] -> Int -> State -> ([Gene], Int, State))
+gmap = Map.fromList [
+    (Add,           add'),
+    (Subtract,      subtract'),
+    (Multiply,      multiply'),
+    (Divide,        divide'),
+    (Incr,          incr'),
+    (Decr,          decr')
     ]
 
-runner =            (toDyn runner',     1 :: Int)
+-- | Gene implementations ------------------------------------------------------
 
-digit1, digit2, digit3 :: Int
-digit1 = 1
-digit2 = 2
-digit3 = 3
+add', subtract', multiply', divide', incr', decr' :: 
+    [Gene] -> Int -> State -> ([Gene], Int, State)
 
-plus, minus, stick :: Int -> Int -> Int
-plus x y = x + y
-minus x y = x - y
-stick x y = read (show x ++ show y) 
+add' gs gi (a, b) = (gs, gi+1, (a+b, 0))
+subtract' gs gi (a, b) = (gs, gi+1, (a-b, 0))
+multiply' gs gi (a, b) = (gs, gi+1, (a*b, 0))
+divide' gs gi (a, 0) = (gs, gi+1, (0, 0))
+divide' gs gi (a, b) = (gs, gi+1, (quot a b, 0))
+incr' gs gi (a, b) = (gs, gi+1, (a, b+1))
+decr' gs gi (a, b) = (gs, gi+1, (a, min 0 (b-1)))
 
-switch :: Int -> Int -> Int -> Int
-switch a b x 
-    | x > 0 = a
-    | otherwise = b
+-- | Scoring function ----------------------------------------------------------
 
-{-
-runner :: (Int -> Int) -> Float
-runner f = fromIntegral (f 100)
--}
-
-runner' :: Int -> Float
-runner' x = fromIntegral $ abs (x - targetPhenotype) 
-
-runner'' :: Int -> Float
-runner'' x = fromIntegral x
+score :: [Gene] -> Float
+score gs = score' $ eval gs gmap 0 initState
+    where score' = fromIntegral . fst
+          initState = (0, 0)
 
--- tests/Tests.hs
+++ tests/Tests.hs
@@ -1,24 +1,13 @@
 -- | First attempt at haskell unit tests.
 import Beagle.Domain
-import Beagle.Eval
 import Test.HUnit
 
--- | Syntactic shortcut for evaluating genotype.
-f = snd . eval
-
 tests = test [
-    "Single gene" ~: 1 ~=? f [Digit1, Runner''],
-    "Basic function" ~: 4 ~=? f [Digit1, Digit3, Plus, Runner''],
-    "Bad basic function" ~: 1 ~=? f [Digit3, Plus, Runner''],
-    "2 functions" ~: 15 ~=? f [Digit2, Digit1, Stick, Digit3, Plus, Runner''],
-    "Basic if" ~: 3 ~=? f [Digit1, Digit2, Digit3, Switch, Runner''],
-    "If with function" ~: 4 ~=? 
-        f [Digit2, Digit3, Minus, Digit3, Digit2, 
-           Digit2, Plus, Switch, Runner''],
-    "If with runner" ~: 1 ~=?
-        f [Digit2, Digit1, Switch, Runner],
-    "Basic runner'" ~: 0 ~=? f [Digit2, Digit3, Plus, Runner'],
-    "Basic runner', slightly off" ~: 3 ~=? f [Digit2, Runner']
+    3 ~=? score [Incr, Incr, Incr, Add],
+    0 ~=? score [Add],
+    0 ~=? score [Incr],
+    2 ~=? score [Incr, Incr, Incr, Add, Incr, Subtract],
+    0 ~=? score [Divide]
     ]
 
 main = runTestTT tests
