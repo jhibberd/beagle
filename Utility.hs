d95f0b1331b9367aa2bc01d15fa48e69ae2c03f9
James Hibberd <james.d.hibberd@gmail.com>
2012-06-19 14:22:52 -0700

    Beginnings of fitness function.

--- Utility.hs
+++ Utility.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE BangPatterns #-}
+
 import Data.List
 import qualified Data.Map as Map
 import Data.Maybe
 import Prelude hiding (flip)
+import System.Random
 
 type Scenario = [Int] -- 9-dimensional vector representing game scenario
 type Player = Int -- 1 or (-1)
@@ -137,9 +140,11 @@ subsequentScenarios s = let is = immediateScenarios s
                             ss = nub . concat $ map subsequentScenarios is
                         in s:ss
 
-main = print . length $ subsequentScenarios [0, 0, 0, 0, 0, 0, 0, 0, 0]
+--main = print . length $ subsequentScenarios [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 -- HASH TABLE ------------------------------------------------------------------
+
+type Alleles = [Int]
 
 -- | Express a scenario as an int, for use as a hash table key.
 toKey :: Scenario -> Int
@@ -152,32 +157,123 @@ buildTable :: Map.Map Int Int
 buildTable = Map.fromList $ zip scenarioKeys [0..] 
     where scenarioKeys = map toKey . subsequentScenarios $ replicate 9 0
 
--- | Return the gene index associated with a scenario.
--- TODO(jhibberd) This function should take a base, not any scenario
+-- | Return the gene index associated with a base scenario.
 scenarioIndex :: Scenario -> Int
-scenarioIndex s = case Map.lookup k buildTable of
+scenarioIndex s = case Map.lookup (toKey s) buildTable of
                       (Just i) -> i
                       Nothing ->  error "Illegal scenario"
-    where k = toKey . fst $ toBase s
-{-
-1. Get scenario
-2. Get base and restore f
-3. Get alleles index
-4. Get solution index
-5. Apply solution to base
-6. apply restore f to modified f
--}
 
--- TODO
-{-
-type Alleles = [Int]
-type Genome = Map.Map Int Int
-f :: Genome -> Alleles -> Scenario -> Scenario
-f g a s = scenarioIndex s
--}
+-- | Using a list of alleles (genetically evolved) solve a scenario by placing
+-- the optimum next move.
+--
+-- First convert the scenario to its base case (and restore function), then
+-- lookup the optimum move for the base case in the alleles list, apply the
+-- optimum move then transform the modified base case to its original state
+-- (using the restore function).
+solve :: Alleles -> Scenario -> Scenario
+solve al scn = let (base, restore) = toBase scn
+                   idx = scenarioIndex base
+                   solution = al !! idx
+                   scn' = play scn solution
+                   scn'' = restore scn'
+               in scn''
 
 {-
+mockAlleles = replicate 627 3
+
 main = do
-    let t = buildTable
-    print $ scenarioIndex [0, 0, 0, 1, 1, 0, 0, (-1), 0]
+    let solve' = solve mockAlleles
+    print $ solve' [1, 0, 0, 0, 0, 0, (-1), 0, 0]
 -}
+
+-- RUNNER ----------------------------------------------------------------------
+
+populationSize = 2
+
+-- | Create a list of randomly chosen alleles.
+mkAlleles :: RandomGen g => g -> (Alleles, g)
+mkAlleles = mkAlleles' (Map.size buildTable)
+    where mkAlleles' 0 g = ([], g)
+          mkAlleles' n g = let (x, g') = randomR (0, 8) g
+                               (xs, g'') = mkAlleles' (n-1) g'
+                           in (x:xs, g'')
+
+-- | Generation a population of random alleles.
+mkPopulation :: RandomGen g => g -> ([Alleles], g)
+mkPopulation !g = mkPopulation' populationSize g
+    where mkPopulation' 0 !g = ([], g)
+          mkPopulation' n !g = let (x, g') = mkAlleles g
+                                   (xs, g'') = mkPopulation' (n-1) g'
+                               in (x:xs, g'') 
+
+--main = print $ mkPopulation (mkStdGen 0)
+
+-- FITNESS EVALUATION ----------------------------------------------------------
+
+numGames = 100 -- TODO(jhibberd) Replace with system that plays all possible games.
+
+
+-- TODO(jhibberd) Clean up
+hostPlay :: RandomGen g => Scenario -> g -> (Scenario, g)
+hostPlay grid g = let (i, g') = randomR (0, (length freeonly)-1) g
+                      i'' = freeonly !! i
+                      (_, _, (_, grid')) = play X i'' [] 0 (toState grid)
+                  in (grid', g')
+    where zipped = zip grid [0..]
+          freeonly = map snd $ filter ((==N) . fst) zipped
+
+toScore :: [Int] -> Float
+toScore xs = 1 - (1 / (fromIntegral $ (sum xs +1))) 
+
+score :: [Gene] -> IO Float
+score gs = do
+        realScore <- runner'' gs numGames g OTurn
+        return $ toScore realScore
+    where g = mkStdGen 12
+          numGames = 100
+
+runner'' :: (RandomGen g) 
+         => [Gene] 
+         -> Int 
+         -> g 
+         -> Turn 
+         -> IO [Int]
+runner'' _ 0 _ _ = return []
+runner'' gs n g t = do
+        (outcome, g') <- playGame gs newGrid g 0 t
+        outcome' <- runner'' gs (n-1) g' nextT
+        return (outcome:outcome')
+    where newGrid = toState (replicate 9 N)
+          nextT = case t of
+                      XTurn -> OTurn
+                      OTurn -> XTurn
+
+
+-- TODO Need bool indicating which mark the genome is playing as.
+playGame :: (RandomGen g) 
+         => (Scenario -> Scenario) -- bot 
+         -> Scenario 
+         -> g
+         -> IO (Bool, g)
+playGame gnm s g
+    | isWinner s =                  return (True, g) -- TODO(jhibberd) Player X changes from bot to host!
+    | isWinner $ invertPlayers s =  return (False, g) -- See above
+    | isDraw s =                    return (True, g)
+    | otherwise = 
+          case (detectPlayer s) of
+
+              1 -> do
+                  let (s', g') = hostPlay s g
+                  playGame gnm s' g'
+
+              (-1) -> do
+                  let s' = gnm s
+                  if isLegalMove s s'
+                      then playGame gnm s' g
+                      else return (False, g) -- illegal move, so lose the game
+
+-- | Return whether a change from a 'before' scenario to an 'after' scenario
+-- represents a legal move.
+isLegalMove :: Scenario -> Scenario -> Bool
+isLegalMove before after = --TODO (length $ filter (/=N) grid) == numTurns 
+
